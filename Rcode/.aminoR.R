35e9c6db30c676543368301a351e5f4c6b7011d8
Alison Chaves <108031197+41ison@users.noreply.github.com>
2023-02-11 01:28:57 +0100

    Update .aminoR.R

--- Rcode/.aminoR.R
+++ Rcode/.aminoR.R
@@ -215,34 +215,11 @@ pKa_plot <- function(x) {
 # it is necessary to have a data.frame containing the variables pKa, pKb, and pKx
 pKa_plot(aminoacids)
 
-
-
-
-
-# pKa dos diferentes aminoácidos para cada região (COOH, NH2 e cadeia lateral)
-# pKa of different amino acids for each group (COOH, NH2 and R-group)
-pKa <- ggplot(aminoacids, aes(x = Abbr)) +
-     geom_line(aes(y = pKa, group = 1), color = "#0073C2FF") +
-        annotate("text", x = 16, y = 2.7, label = "-COOH",
-            color = "#0073C2FF", size = 6) +
-     geom_line(aes(y = pKb, group = 2), color = "#CD534CFF") +
-        annotate("text", x = 15, y = 11, label = "-NH2",
-            color = "#CD534CFF", size = 6) +
-     geom_col(aes(y = pKx, group = 3), fill = "#4A6990FF", alpha = 0.4) +
-        annotate("text", x = 9, y = 6.8, label = "R-group",
-            color = "#4A6990FF", size = 6) +
-     scale_y_continuous(limits = c(0, 12.5), breaks = seq(0, 12.5, 2)) +
-        labs(x = "Aminoacids", y = "pKa",
-             title = "Aminoacids properties") +
-    theme_classic(base_size = 20)
-
-ggsave("pKa.png", pKa, width = 10, height = 6, dpi = 300,
-    path = "/Volumes/Expansion/proteomics_basics/")
-
-# distribuição das massas monoisotópicas dos aminoácidos
-monoiso <- ggplot(aminoacids, aes(x = Abbr, y = Monoisotopic_Mass,
+# function to plot the monoisotopic mass of the aminoacids coloring by hydropathy
+monoiso_plot <- function(x) {
+    ggplot(aminoacids, aes(x = Abbr, y = Monoisotopic_Mass,
                     size = Monoisotopic_Mass, color = Hydropathy)) +
-                scale_color_manual(values = c("#0073C2FF", "#CD534CFF", "#8F7700FF")) +
+            scale_color_manual(values = c("#0073C2FF", "#CD534CFF", "#8F7700FF")) +
     geom_point(alpha = 0.5) +
     guides(colour = "legend", size = FALSE) +
     geom_segment(aes(x = Abbr, xend = Abbr,
@@ -253,14 +230,18 @@ monoiso <- ggplot(aminoacids, aes(x = Abbr, y = Monoisotopic_Mass,
         color = "#868686FF", size = 5, nudge_x = 2,
             nudge_y = 2, show.legend = FALSE) +
     scale_size(range = c(6, 20)) +
-        scale_y_continuous(limits = c(50, 190),
+    scale_y_continuous(limits = c(50, 190),
         breaks = seq(50, 190, 10)) +
     labs(x = "Aminoacid", y = "Monoisotopic mass",
          title = "Monoisotopic mass and charge of aminoacids") +
     theme_classic(base_size = 20) +
     theme(axis.text.x = element_text(angle = 90,
-                        vjust = 0.5, hjust = 1), legend.position = "bottom") +
-                        facet_wrap(~ Hydropathy, ncol = 3, scales = "free_x")
+                    vjust = 0.5, hjust = 1), legend.position = "bottom") +
+                    facet_wrap(~ Hydropathy, ncol = 3, scales = "free_x")
 
 ggsave("Monoisotopic_Mass.png", monoiso, width = 9, height = 8, dpi = 300,
     path = "/Volumes/Expansion/proteomics_basics/")
+}
+
+# using the function for plot monoisotopic masses and hydropathy
+monoiso_plot(aminoacids)
